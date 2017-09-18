f36136a8348d72bd1817461f13749f693458c880
janeligio <jeligio808@gmail.com>
2017-09-17 21:30:51 -1000

    part 2

---
+++ ziplist.js
@@ -0,0 +1,8 @@
+/*
+Write a function called zipList that accepts two lists of equal length and returns the result of alternatingly taking elements. For example: given the two lists [‘a’, ‘b’, ‘c’] and [1, 2, 3], the function should return [‘a’, 1, ‘b’, 2, ‘c’, 3]. zipList should not use Underscore. Now write a function called zipListTheSimpleWay that does the same thing using Underscore.
+*/
+
+function zipList(list1, list2) {
+
+}
+
