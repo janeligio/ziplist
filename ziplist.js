7b4aaaaff2451774622e820b560573c533d4af88
janeligio <jeligio808@gmail.com>
2017-09-18 09:14:05 -1000

    finished

--- ziplist.js
+++ ziplist.js
@@ -3,6 +3,21 @@ Write a function called zipList that accepts two lists of equal length and retur
 */
 
 function zipList(list1, list2) {
+  let arr = [];
+  for(let i = 0; i < list1.length; i++){
+    arr.push(list1[i]);
+    arr.push(list2[i]);
+  }
+  return arr;
 
 }
 
+let myarr = [1, 2, 3];
+let myarr2 = ["a", "b", "c"];
+console.log(zipList(myarr, myarr2));
+
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+
+console.log(zipListTheSimpleWay(myarr, myarr2));
